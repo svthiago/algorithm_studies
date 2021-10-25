54f5b692e234ee8a869408fd0cef1f60c3a64e4c
Thiago Vilela <thiago.vilela@olist.com>
2021-10-25 15:37:04 -0300

    Permutation missing element

---
+++ lessons/time_complexity/perm_missing_element.py
@@ -0,0 +1,18 @@
+# O(n) or O(n * log(n)) 
+# performance improvement for later
+
+def solution(A):
+
+    B = list(range(1, len(A)+1))
+    if len(A) == 1:
+        B = [1, 2]
+
+    if len(A) == 0:
+        return 1
+
+    C = set(B) - set(A)
+
+    if len(C) == 0:
+        return len(A) + 1
+
+    return C.pop()
