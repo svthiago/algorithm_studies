49058d3de3ed146fe4b984a97d5098e7b2e611fe
Thiago Vilela <thiago.sv9@gmail.com>
2022-01-01 23:45:15 -0300

    removing code out of context

--- lessons/counting_elements/perm_check.py
+++
@@ -1,9 +0,0 @@
-def solution(A):
-    n = len(A)
-    s = set(A)
-    cs = set(list(range(1, n+1)))
-
-    if cs.issubset(s):
-        return 1
-    else:
-        return 0
--- lessons/time_complexity/frog_jump.py
+++
@@ -1,7 +0,0 @@
-import math
-
-def solution(X, Y, D):
-    G = Y - X
-    J = math.ceil(G / D)
-
-    return J
