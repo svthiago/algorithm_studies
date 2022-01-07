64485a07f5000a17feca58b68a1aee341fbe85ef
Thiago Vilela <thiago.sv9@gmail.com>
2022-01-06 22:41:56 -0300

    two sum

--- leet_code/arrays/two_sum.py
+++ leet_code/arrays/two_sum.py
@@ -1,6 +1,10 @@
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in nums:
-            for j in nums:
-                if (i + j) == target:
-                    return [nums.index(i), nums.index(j)]
+        d = {}
+        for i, n in enumerate(nums):
+            f =  target - n
+            if f in d.keys():
+                return [i, d[f]]
+            else:
+                d[n] = i
