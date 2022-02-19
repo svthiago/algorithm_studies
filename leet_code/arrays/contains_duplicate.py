34473a68cd2be849ad4fa414df21523a8cc35643
Thiago Vilela <thiago.sv9@gmail.com>
2022-02-19 19:44:40 -0300

    removed old folder

--- leet_code/arrays/contains_duplicate.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        n1 = len(nums)
-        n2 = len(set(nums))
-        
-        if n1 != n2:
-            return True
-        return False
